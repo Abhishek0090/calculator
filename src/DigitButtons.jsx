0a1d3640accba1a6bde0697cc7f445b979c988f1
Abhishek0090 <palabhishek411@gmail.com>
2023-09-17 22:10:11 +0530

    Deployed

---
+++ src/DigitButtons.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ACTIONS } from "./App";
+
+const DigitButtons = ({ dispatch, digit }) => {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+};
+
+export default DigitButtons;
